35c1fe3b84daad0d5113d9ce82bb336d674511b3
Tobias Müller <tm086@hdm-stuttgart.de>
2018-07-03 14:22:32 +0200

    Change Styles from CSS to SCSS // Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './details/details.component';
+import { VisualComponent } from './visual/visual.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DetailsComponent,
+    VisualComponent
   ],
   imports: [
     BrowserModule
