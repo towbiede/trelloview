db54b3fc71822a9051759e6970bbfdcafc864f88
Tobias Müller <tm086@hdm-stuttgart.de>
2018-07-04 14:15:35 +0200

    Create getData() function to get Data from Trelo with ApiKey

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  private apiKey = '4c96a328e7ea9b34311ac6dade4d0b8d';
+  private apiToken = '20b32bb9f9a12cb3d431232af241f628dbe5e661d340630928153984cc6b81ea';
+  private apiURL = 'https://api.trello.com/1/members/me/boards?key=' + this.apiKey + '&token=' + this.apiToken;
+
+  data: any = {};
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getData() {
+    return this.http.get(this.apiURL).subscribe(data => {
+      console.log(data);
+    });
+  }
 
   ngOnInit() {
+    this.getData();
   }
 
 }
