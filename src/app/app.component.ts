37728da4a20e0bffabc13c3ef799eb3ba211f1f8
Tobias Müller <tm086@hdm-stuttgart.de>
2018-07-05 14:13:14 +0200

    Outcomment unused vars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'TrelloView';
 
-  public apiKey = '4c96a328e7ea9b34311ac6dade4d0b8d';
-  private apiToken = '20b32bb9f9a12cb3d431232af241f628dbe5e661d340630928153984cc6b81ea';
-
+  /**
+   * public apiKey = '4c96a328e7ea9b34311ac6dade4d0b8d';
+   * private apiToken = '20b32bb9f9a12cb3d431232af241f628dbe5e661d340630928153984cc6b81ea';
+   * */
 }
