d085d00e4e9af6943967be20333850378c580a3d
Tobias Müller <tm086@hdm-stuttgart.de>
2018-07-05 13:19:59 +0200

    import HttpClient and define constructor

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-details',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private http = HttpClient) { }
+
+
 
   ngOnInit() {
   }
