44de85ba1950d02187c55404d668fbf7e823b0dc
Tobias Müller <tm086@hdm-stuttgart.de>
2018-07-05 16:03:34 +0200

    remove import of deleted board class

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Board } from '../board';
 
 @Component({
   selector: 'app-dashboard',
