68be6e2c4bbd851564729934399f7a0d64126a22
SchelleLeon <ls104@hdm-stuttgart.de>
2018-07-19 20:14:05 +0200

    trello.d.ts created, Json to typescript with json2ts

---
+++ src/app/trello.d.ts
@@ -0,0 +1,89 @@
+/**
+ * Contents of this file where created using http://json2ts.com
+ *
+ * You probably should extend this file. Whenever you call a new object from
+ * Trellos API paste the JSON into json2ts and extend this Trello namespace.
+ * */
+export namespace Trello {
+  export interface User {
+    id: string;
+    avatarHash: string;
+    avatarUrl: string;
+    bio: string;
+    bioData: BioData;
+    confirmed: boolean;
+    fullName: string;
+    idEnterprisesDeactivated: any[];
+    idPremOrgsAdmin: any[];
+    initials: string;
+    memberType: string;
+    products: any[];
+    status: string;
+    url: string;
+    username: string;
+    avatarSource: string;
+    email: string;
+    gravatarHash: string;
+    idBoards: string[];
+    idEnterprise?: any;
+    idOrganizations: string[];
+    idEnterprisesAdmin: any[];
+    limits: Limits;
+    loginTypes: string[];
+    marketingOptIn: MarketingOptIn;
+    messagesDismissed: any[];
+    oneTimeMessagesDismissed: string[];
+    prefs: Prefs;
+    trophies: any[];
+    uploadedAvatarHash: string;
+    uploadedAvatarUrl: string;
+    premiumFeatures: any[];
+    idBoardsPinned?: any;
+  }
+
+  export interface Emoji {
+  }
+
+  export interface BioData {
+    emoji: Emoji;
+  }
+
+  export interface TotalPerMember {
+    status: string;
+    disableAt: number;
+    warnAt: number;
+  }
+
+  export interface Boards {
+    totalPerMember: TotalPerMember;
+  }
+
+  export interface TotalPerMember2 {
+    status: string;
+    disableAt: number;
+    warnAt: number;
+  }
+
+  export interface Orgs {
+    totalPerMember: TotalPerMember2;
+  }
+
+  export interface Limits {
+    boards: Boards;
+    orgs: Orgs;
+  }
+
+  export interface MarketingOptIn {
+    optedIn: boolean;
+    date: Date;
+  }
+
+  export interface Prefs {
+    sendSummaries: boolean;
+    minutesBetweenSummaries: number;
+    minutesBeforeDeadlineToNotify: number;
+    colorBlind: boolean;
+    locale: string;
+  }
+
+}
