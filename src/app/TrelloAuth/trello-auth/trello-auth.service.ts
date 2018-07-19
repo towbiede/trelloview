47df58660f8855b2c88cbbad2bb880bf3b53bed7
SchelleLeon <ls104@hdm-stuttgart.de>
2018-07-19 19:36:03 +0200

    trello-auth.service mit getToken() und login()

--- src/app/TrelloAuth/trello-auth/trello-auth.service.ts
+++ src/app/TrelloAuth/trello-auth/trello-auth.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
+import {TRELLO_TOKEN_KEY} from "./trello-token-key";
+import {Promise} from "q";
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable( )
 export class TrelloAuthService {
 
-  constructor() { }
+
+
+  public token: string;
+  public apiKey: string = 'key';
+
+  constructor(private  router: Router) {
+    this.token = localStorage.getItem(TRELLO_TOKEN_KEY);
+  }
+
+
+
+
+
+  getToken(): string | undefined {
+    return localStorage.getItem(TRELLO_TOKEN_KEY);
+  }
+
+setToken(token:string) : void {
+    return localStorage.setItem(TRELLO_TOKEN_KEY, token)
 }
+  login(){
+    window.location.href = this.assembleUrl();
+  }
+
+
+  assembleUrl() : string {
+    const returnUrl = encodeURI (  window.location.href + 'set-token');
+    return 'placeholder'
+  }
+
+  /**
+   * Logout, guards are responsible to redirect
+   */
+
+  async logout(): Promise<void>{
+    localStorage.removeItem(TRELLO_TOKEN_KEY);
+    await this.router.navigate(['/']);
+  }
+
+}
+
+
+
