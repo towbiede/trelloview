67b74a1f287fe41396315ebf6c9348fca01b6d62
SchelleLeon <ls104@hdm-stuttgart.de>
2018-07-19 20:40:10 +0200

    import TrelloAuthService in trello-auth.module.ts

--- src/app/TrelloAuth/trello-auth/trello-auth.module.ts
+++ src/app/TrelloAuth/trello-auth/trello-auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TrelloAuthService} from "./trello-auth.service";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  providers: [
+    TrelloAuthService,
+  ]
 })
 export class TrelloAuthModule { }
