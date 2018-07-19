9ccb3dc036c3518e0ee6e15bb52b7f33b8ae893a
SchelleLeon <ls104@hdm-stuttgart.de>
2018-07-19 20:12:21 +0200

    trello.service.ts angepasst. getMe() return User

--- src/app/TrelloAuth/trello-api/trello.service.ts
+++ src/app/TrelloAuth/trello-api/trello.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Trello} from "../../trello";
+import User = Trello.User;
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable( )
 export class TrelloService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  async getMe() : Promise<User>{
+    return this.httpClient.get<User>('https://api.trello.com/1/members/me').toPromise();
+  }
+
+
 }
