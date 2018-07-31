10a262c87fb2f5948a7af29f4dc3c893737498a4
Tobias Müller <tm086@hdm-stuttgart.de>
2018-07-31 17:41:50 +0200

    Correct Trello Get Methoden from hardcoded APIKey

--- src/app/private/dashboard/dashboard.component.ts
+++ src/app/private/dashboard/dashboard.component.ts
@@ -18,20 +18,29 @@ export class DashboardComponent implements OnInit {
   data: any = {};
   boards: any;
   cards: any;
+  lists: any;
 
   constructor(private http: HttpClient, private trelloAuthService: TrelloAuthService, private trelloService: TrelloService) {}
 
   getBoards() {
-    return this.http.get(this.apiURL).subscribe(data => {
+    return this.http.get('https://api.trello.com/1/members/me/boards').subscribe(data => {
       this.boards = data;
     });
   }
 
   getCardsByBoardId(boardId: string) {
     this.bId = boardId;
-    const getCardsUrl = 'https://api.trello.com/1/boards/' + this.bId + '/cards?key=' + this.apiKey + '&token=' + this.apiToken;
+    const getCardsUrl = 'https://api.trello.com/1/boards/' + this.bId + '/cards'
     return this.http.get(getCardsUrl).subscribe(data => {
       this.cards = data;
+    });
+  }
+
+  getListsByBoardId(boardId: string) {
+    this.bId = boardId;
+    const getListsUrl = 'https://api.trello.com/1/boards/' + this.bId + '/lists'
+    return this.http.get(getListsUrl).subscribe(data => {
+      this.lists = data;
     });
   }
 
